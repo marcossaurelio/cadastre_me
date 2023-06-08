fba09251f9c1cb0edbbe3a1dcd5d3f17abcf3bca
Marcos Aurélio <aurelio257@outlook.com>
2023-06-08 14:44:25 -0300

    Banco de dados configurado e funcionando, os dados registrados estão sendo salvos no banco interno da aplicação; Arquivo base de estilização configurado.

--- app_cad_usuarios/views.py
+++ app_cad_usuarios/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import Usuario
 
 def home(request):
     return render(request,'usuarios/home.html')
 
 def usuarios(request):
-    pass
+    
+    # Salvar os dados da tela para o banco de dados.
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    
+    # Exibir todos os usuários já cadastrados em uma nova página.
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    
+    #retornar os dados para a página de listagem de usuários.
+    return render(request,'usuarios/usuarios.html',usuarios)

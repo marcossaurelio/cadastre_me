840a80ec365cb8171627bba5675be53e348b13de
Marcos Aurélio <aurelio257@outlook.com>
2023-06-08 11:22:58 -0300

    Campos criados no banco de dados dentro do arquivo models.py

--- projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/urls.py
@@ -3,5 +3,6 @@ from app_cad_usuarios import views
 
 urlpatterns = [
     # rota, view responsável, nome de referência
-    path('',views.home,name='home'),
+    path('',views.home,name="home"),
+    path('usuarios/',views.usuarios,name="listagem_usuarios")
 ]
